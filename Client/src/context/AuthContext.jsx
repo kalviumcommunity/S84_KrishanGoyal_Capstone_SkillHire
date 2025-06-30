092548b2c970b0b3975eb22c78d6a4a74015771a
krishangoyal12 <krishangoyal717@gmail.com>
2025-06-30 15:44:22 +0530

    fixed

--- Client/src/context/AuthContext.jsx
+++ Client/src/context/AuthContext.jsx
@@ -6,7 +6,7 @@ const AuthContext = createContext();
 export const useAuth = () => {
     const context = useContext(AuthContext);
     if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider');
+        throw new Error('useAuth must be used inside an AuthProvider');
     }
     return context;
 };
